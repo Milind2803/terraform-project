11ca3578f9eb83509570537f90c3333c32afb5e4
Milind2803 <95535585+Milind2803@users.noreply.github.com>
2026-03-18 12:44:16 +0530

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,107 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_iam_role" "cluster" {
+  name = "eks-cluster-example"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sts:AssumeRole",
+          "sts:TagSession"
+        ]
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_eks_cluster" "cluster" {
+  name = "cluster"
+  access_config {
+    authentication_mode = "API"
+    }
+  role_arn = aws_iam_role.cluster.arn
+  vpc_config {
+    subnet_ids = data.aws_subnets.default.ids
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
+  ]   
+}
+
+resource "aws_eks_node_group" "node" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "node"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids = data.aws_subnets.default.ids
+
+ scaling_config {
+  desired_size = 2
+  min_size     = 1
+  max_size     = 3
+}
+
+ ami_type = "AL2023_x86_64_STANDARD"
+    instance_types = ["c7i-flex.large"]
+    capacity_type = "ON_DEMAND"
+    disk_size = 20
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_iam_role" "node" {
+  name = "eks-node-group-node"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node.name
+}
